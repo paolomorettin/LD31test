e3fa373e2dd699d5cfb2b790e8e398f2392b9135
paolomorettin <morettin.paolo@gmail.com>
2014-11-26 00:28:55 +0100

    parsaawawaw

--- paolo/COINFLIP.py
+++ paolo/COINFLIP.py
@@ -1,5 +1,12 @@
 
-class COINFLIP_Parser :
+class CP_Exception (Exception) :
+    def __init__(self,msg) :
+        self.msg = msg
+
+    def __str__(self) :
+        return repr(self.msg)
+
+class CP_Parser :
 
     special_characters = [';','(',')','=',',']
 
@@ -14,12 +21,82 @@ class COINFLIP_Parser :
 
     @staticmethod
     def parse_code(code) :
-        tokens = COINFLIP_Parser.tokenize(code)
+        tokens = CP_Parser.tokenize(code)
+        program = []
+        i = 0
+        j = 0
+        while j < len(tokens) :
+            if j == ';' :
+                program.append(CP_Parser.parse_decl(tokens[i:j-1]))
+                i = j + 1
+            j += 1
 
-        program = ''
-        declaration = []
-        for t in tokens :
-            if t == ';' :
-                program += COINFLIP_Parser(
+        return program
             
-        
+    @staticmethod
+    def parse_decl(tokens) :
+        if tokens[0] != 'IF' or tokens[-2] != 'THEN' :
+            raise CP_Exception("Incorrect declaration")
+        cond =  CP_Parser.parse_cond(tokens[1:-2])
+        action = CP_Parser.parse_action(tokens[-1])
+        return cond,action
+    
+    @staticmethod
+    def parse_cond(tokens) :
+        if len(tokens) == 0 :
+            raise CP_Exception("Incorrect condition")
+
+        elif 'AND' in tokens or 'OR' in tokens :
+            subcond = []
+            lvl = 0
+            res = ""
+            for t in tokens :
+                if lvl == 0 and (t == 'OR' or t == 'AND') :
+                    if subcond[0] == '(' and subcond[-1] == ')' :
+                        res += "( {} ) {} ".format(CP_Parser.parse_cond(subcond[1:-1]),t.lower())
+                    else :
+                        res += "{} {} ".format(CP_Parser.parse_cond(subcond),t.lower())
+                    subcond = []
+                else :
+                    if t == '(' :
+                        lvl += 1
+                    elif t == ')' :
+                        lvl -= 1
+                    subcond.append(t)
+            if subcond[0] == '(' and subcond[-1] == ')' :
+                res += "({})".format(CP_Parser.parse_cond(subcond[1:-1]))
+            else :
+                res += CP_Parser.parse_cond(subcond)
+            return res
+
+        elif tokens[0] == 'NOT' :
+            return "not {}".format(CP_Parser.parse_cond(tokens[1:]))
+        elif tokens[0] == '(' and tokens[-1] == ')' :
+            return "({})".format(CP_Parser.parse_cond(tokens[1:-1]))
+        else :
+            try :
+                s,op,v1,c,v2,cp,eq,tr = tokens
+                if [s,op,c,cp,eq] == ['SENSOR','(',',',')','='] and tr in ['WALL','ROBOT','FLOOR'] :
+                    return "self.sensor({},{}) == '{}'".format(str(int(v1)),str(int(v2)),tr)
+            except ValueError as e :
+                raise CP_Exception("Incorrect condition "+str(e))
+
+    @staticmethod
+    def parse_action(tokens) :
+        if len(tokens) == 1 :
+            t = tokens[0]
+            if t == 'WAIT' or t == 'SHOOT' :
+                return "self.action = '{}'".format(t)
+            else :
+                raise CP_Exception("Incorrect action")
+
+        elif len(tokens) == 2 :
+            t1,t2 = tokens
+            if (t1 == 'MOVE' and t2 in ['FORWARD','BACKWARD']) or (t1 == 'TURN' and t2 in ['LEFT','RIGHT']) :
+                return "self.action = '{} {}'".format(t1,t2)
+            else :
+                raise CP_Exception("Incorrect action")
+            
+        else :
+            raise CP_Exception("Incorrect action")
+
