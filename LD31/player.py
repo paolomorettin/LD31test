aaf5afe455b72a01f8e5c2883cc0482fcbaf2db9
manoet <manoet1@hotmail.it>
2014-12-06 16:32:52 +0100

    Now player keeps moving when keys are kept pressed

--- LD31/player.py
+++ LD31/player.py
@@ -13,6 +13,8 @@ class Player(cocos.layer.ColorLayer):
         self.cell_y = 0
         self.sprite.position = self._get_drawing_coors()
         self.add(self.sprite)
+        self.curr_action = None
+        self.schedule_interval(self.update, 0.5)
 
     def _get_drawing_coors(self):
         base_x = maplayer.MapLayer.SPRITE_SIZE * self.cell_x
@@ -23,17 +25,27 @@ class Player(cocos.layer.ColorLayer):
     def _movement_allowed(self, direction):
         return self.game.get_cell(self.cell_x, self.cell_y).wall[direction] == 0
 
-    def on_key_press(self, key, modifiers):
-        if key == pyglet.window.key.LEFT and self._movement_allowed(gamelogic.DIRECTION_LEFT):
+    def update(self, timedelta):
+        if self.curr_action == pyglet.window.key.LEFT and self._movement_allowed(gamelogic.DIRECTION_LEFT):
             self.cell_x -= 1
-        elif key == pyglet.window.key.UP and self._movement_allowed(gamelogic.DIRECTION_UP):
+        elif self.curr_action == pyglet.window.key.UP and self._movement_allowed(gamelogic.DIRECTION_UP):
             self.cell_y += 1
-        elif key == pyglet.window.key.DOWN and self._movement_allowed(gamelogic.DIRECTION_DOWN):
+        elif self.curr_action == pyglet.window.key.DOWN and self._movement_allowed(gamelogic.DIRECTION_DOWN):
             self.cell_y -= 1
-        elif key == pyglet.window.key.RIGHT and self._movement_allowed(gamelogic.DIRECTION_RIGHT):
+        elif self.curr_action == pyglet.window.key.RIGHT and self._movement_allowed(gamelogic.DIRECTION_RIGHT):
             self.cell_x += 1
+        self.sprite.do(cocos.actions.MoveTo(self._get_drawing_coors(), 0.25))
+        self.game.enter_cell(self.cell_x, self.cell_y)
 
-        self.sprite.position = self._get_drawing_coors()
+    def on_key_press(self, key, modifiers):
+        if self.curr_action and self.curr_action != key:
+            return
+        valid_keys = {pyglet.window.key.LEFT, pyglet.window.key.UP, pyglet.window.key.DOWN, pyglet.window.key.RIGHT}
+        if key in valid_keys:
+            self.curr_action = key
+
+    def on_key_release(self, key, modifiers):
+        self.curr_action = None
 
 if __name__ == "__main__":
     import sys
@@ -42,6 +54,7 @@ if __name__ == "__main__":
         print "However, if you just want to run a test, use -debug option."
         sys.exit(0)
     cocos.director.director.init()
+    gamelogic.Game.instance().load_from("level.dat")
     player = Player()
     test = cocos.scene.Scene(player)
     cocos.director.director.run(test)
