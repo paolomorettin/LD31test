fe25f28da9f1dbb9b49fe5353ac7cb1efe2a9fee
Dani Ludum Campa <danicampa90@gmail.com>
2014-12-06 14:05:17 +0100

    Game logic stubs.

---
+++ LD31/gamelogic.py
@@ -0,0 +1,44 @@
+
+# directions (enum)
+DIRECTION_UP, DIRECTION_LEFT, DIRECTION_DOWN, DIRECTION_RIGHT = range(4)
+
+# walls: 0 = no wall, 1 = wall
+# style: 0..n = colors
+# type
+CELLTYPE_NORMAL, CELLTYPE_START, CELLTYPE_END = range(3)
+
+# Single cell of the map
+class Cell:
+    def __init__(self):
+        self.wall = [0]*4
+        self.style = 0
+        self.type = CELLTYPE_NORMAL
+
+
+# Game state.
+class Game:
+    _instance = None
+
+    @staticmethod
+    def instance():
+        if Game._instance is None:
+            Game._instance = Game()
+        return Game._instance
+    
+    def __init__(self):
+        print "Creating a new Game."
+
+    def load_from(self, fname):
+        # TODO: Stub
+        pass
+
+    def get_cell(self,x,y):
+        # returns the Cell at (x,y) or None
+        # TODO: Stub
+        return Cell()
+
+    def enter_cell(self,x,y):
+        # called when player enters a cell. May trigger some changes over the map.
+        # returns list of NEW items to put on the map.
+        # TODO: Stub
+        return []
