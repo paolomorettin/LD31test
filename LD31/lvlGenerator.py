4dbc001a1816b5ed3d329019c604b8c3d9a5954e
paolomorettin <morettin.paolo@gmail.com>
2014-12-07 12:06:42 +0100

    tutto hardcoded!

--- LD31/lvlGenerator.py
+++ LD31/lvlGenerator.py
@@ -8,18 +8,56 @@ class Graph :
         self.end_point = end_point
         self.size = size
 
+        # divides the grid in blocks (3x2)
+        self.blocks = []
+        block_coords = [((0,8),(0,9)),((9,16),(0,9)),((17,25),(0,9)),((0,8),(10,19)),((9,16),(10,19)),((17,25),(10,19))]
+        for dx,dy in block_coords :
+            for y in xrange(dy[0],dy[1]+1) :
+                for x in xrange(dx[0],dx[1]+1) :
+                    self.blocks.append( (x,y) )
+
         # generates the path from start_point to end_point with a random walk of length >= min_steps
         ok = False
         while not ok :
             # initializes the graph structure
             self.nodes = {}
-            self.edges = []
+            self.edges = [] 
             for x in xrange(size[0]) :
                 for y in xrange(size[1]) :
                     self.nodes[(x,y)] = {'candidate':True, 'color':'white'}    
     
             ok = self.randomWalk(start_point,end_point) and len(self.edges) >= min_steps
 
+        # TRIGGERS
+        self.triggers = []
+        self.main_path = list(self.edges)
+
+        # for each node in the main path, calculates the blocks that won't be visited again by backward-traversing the path
+        n = end_point
+        blocks_to_visit = {i for i in xrange(len(self.blocks)) if n in self.blocks[i]}
+        # BTWBVA = Blocks That Won't Be Visited Again!! 
+        self.nodes[n]['btwbva'] = list(set(range(len(self.blocks))) - blocks_to_visit)
+        
+        while n != start_point :
+            
+            n = [x for x,y in self.main_path if y == n][0]
+            blocks_to_visit = blocks_to_visit.union({i for i in xrange(len(self.blocks)) if n in self.blocks[i]})
+            self.nodes[n]['btwbva'] = list(set(range(len(self.blocks))) - blocks_to_visit)
+        
+        # assigns a trigger to a btwbva whenever it's possible (that is, the first time a blocks becomes btwbva)
+        blocks_triggered = set()
+        while n != end_point :
+            candidates = [b for b in self.nodes[n]['btwbva'] if not b in blocks_triggered]
+            if len(candidates) > 0 :
+                bt = candidates[0]
+                self.triggers.append( (n,[bt]) )
+                blocks_triggered.add(bt)
+            n = [y for x,y in self.edges if x == n][0]
+
+        # the remaining blocks are triggered by an end_point trigger (end_point block included)
+        blocks_left = list(set(range(len(self.blocks)))-blocks_triggered)
+        self.triggers.append( (n,blocks_left) )
+                
         # generates the misleading paths by connecting leftover nodes to the main path
         while len([n for n in self.nodes if self.nodes[n]['color'] == 'white']) > 0 :
             nodes_in_path = [n for n in self.nodes if self.nodes[n]['color'] != 'white' and len([y for y in self.neighbors(n) if self.nodes[y]['color'] == 'white'])>0]
@@ -114,22 +152,39 @@ class Graph :
 
 
 if __name__ == '__main__' :
-    g = Graph((26,20),(0,0),(25,19))
-    
-    matrix = g.graphToMatrix()
-    s = ''
-    for y in xrange(max(map(lambda x:x[1],matrix.keys()))) :
-        for x in xrange(max(map(lambda x:x[0],matrix.keys()))) :
-        
-            if matrix[(x,y)] == 0 :
-                s += ' '
-            else : 
-                s += '#'
-        s += '\n'
-        
-    g = GameMapData()
-    l = LevelData()
-    l.matrix = matrix
-    g.levels[0] = l
-    g.save("level.dat")
-    print s
+
+    game_data = GameMapData()
+    starting_points = [(0,0),(25,19),(0,19),(25,0),(0,0)]
+    lovely_hardcoded_size = (26,20)
+
+    for i in xrange(4) :
+        sp = starting_points[i]
+        ep = starting_points[i+1]
+        g = Graph(lovely_hardcoded_size,sp,ep)
+        matrix = g.graphToMatrix()
+
+        s = 'LEVEL {}\n'.format(str(i))
+        for y in xrange(max(map(lambda x:x[1],matrix.keys()))) :
+            for x in xrange(max(map(lambda x:x[0],matrix.keys()))) :
+                if matrix[(x,y)] == 0 :
+                    s += ' '
+                else : 
+                    s += '#'
+            s += '\n'
+
+        print s
+
+        l = LevelData()
+        # so map
+        l.matrix = matrix
+        # such triggers
+        for coord,id_b in g.triggers :
+            l.triggers[coord] = TriggerData(id_b,i+1)
+
+        game_data.levels.append(l)
+
+    game_data.save("level.dat")
+
+
+
+
