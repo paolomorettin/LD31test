fa4489e7c5f2af7b6daaf74df0b245f6f7c083cd
manoet <manoet1@hotmail.it>
2014-12-07 17:12:42 +0100

    Splash screen

--- LD31/main.py
+++ LD31/main.py
@@ -4,15 +4,26 @@ import maplayer
 import player
 import pyglet
 
+class SplashScreen(cocos.scene.Scene):
+    def __init__(self, main_scene):
+        splash_layer = cocos.layer.Layer()
+        logo = cocos.sprite.Sprite("img/logo.png")
+        logo.position = 400, 300
+        splash_layer.add(logo)
+        super(SplashScreen, self).__init__(splash_layer)
+        cocos.audio.pygame.music.load("sounds/splash.wav")
+        cocos.audio.pygame.music.play(0)
+        self.schedule_interval(self.close, 3.5, main_scene)
+
+    def close(self, timedelta, scene):
+        cocos.audio.pygame.music.load("sounds/music.wav")
+        cocos.audio.pygame.music.play(-1)
+        cocos.director.director.replace(scene)
+
+
 if __name__ == "__main__":
     cocos.director.director.init(width=800, height=600)
     cocos.audio.pygame.mixer.init()
-
-    try:
-        cocos.audio.pygame.music.load("sounds/music.wav")
-        cocos.audio.pygame.music.play(-1)
-    except Exception:
-        print "Unable to play music"
 
     game = gamelogic.Game.instance()
     game.load_from("level.dat")
@@ -24,4 +35,6 @@ if __name__ == "__main__":
 
     main_scene = cocos.scene.Scene(viewer)
     main_scene.add(game.player)
-    cocos.director.director.run(main_scene)
+    splash = SplashScreen(main_scene)
+    cocos.director.director.run(splash)
+
